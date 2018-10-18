a40a59bd6dcb3f06b776c2bacbb36cc4c2e049c7
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-10-17 23:43:29 -0500

    Initial Commit
    Initial Commit

---
+++ q_agent.py
@@ -0,0 +1,58 @@
+# agent_DQN:    load a set of pretrained dcn to estimate the reward of each action
+#               to perform and use the most rewarding action in an Gym forex
+#               broker acccount simulation environment step
+
+from gym.envs.registration import register
+import gym
+import sys
+import neat
+import os
+
+## \class QAgent
+## \brief Q-Learning agent that uses an OpenAI gym environment for fx trading 
+##  This agent has separate networks (Pre-trained DeepConvNets) for estimating 
+##  each action per step of the simulation environment.
+class QAgent():
+    
+    ## init method
+    ## Loads the training and validation datasets, loads the pre-trained models
+    #  initialize forex environment.
+    def __init__(self, key):
+        # First argument is the training dataset
+        ts_f = sys.argv[1]
+        # Second is validation dataset 
+        vs_f = sys.argv[2]
+        # Third argument is the prefix (including path) for the dcn pre-trained models 
+        # for the actions, all modes are files with .model extention and the prefix is
+        # concatenated with a number indicating the action:
+        # 0 = open - buy action
+        # 1 = open - sell action
+        # 2 = not-open-buy action
+        # 3 = not-open-sell action
+        # 4 = close-buy action
+        # 5 = close-sell action
+        # 6 = not-close-buy action
+        # 7 = not-close-sell action
+        model_prefix = sys.argv[2]
+
+    ## Load  pretrained models
+    def load_action_models(self):
+        test=0
+    
+    ## Evaluate all the action models and select the one with most predicted reward
+    def decide_next_action(self):
+        test=0
+    
+    ## Evaluate all the action models and select the one with most predicted reward
+    def decide_next_action(self):
+        test=0
+    
+    ## Evaluate all the steps on the simulation choosing in each step the best 
+    ## action, given the observations per tick. 
+    ## \returns the final balance and the cummulative reward
+    def evaluate(self):
+        test=0       
+
+# main function 
+if __name__ == '__main__':
+    evaluate()
