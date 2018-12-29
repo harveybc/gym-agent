de9713c2cf3e66494ac08acc33d7da3644ead03b
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-12-29 00:55:20 -0500

    Update q_agent_svm.py

--- q_agent_svm.py
+++ q_agent_svm.py
@@ -60,7 +60,7 @@ class QAgent():
         num_ticks = len(n_data)
         self.num_columns = len(n_data[0])
         for i in range(0, self.num_columns-4):
-            header_cell = n_data[0,i]
+            header_cell = n_data[1,i]
             print("header_cell = ", header_cell)
             data = header_cell.split("_")
             num_parts = len(data)
