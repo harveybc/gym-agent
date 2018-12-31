ce0b463e335ad7760f1be4dab93a2bea5d301dd7
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-12-31 03:11:25 -0500

    Update q_agent_svm.py

--- q_agent_svm.py
+++ q_agent_svm.py
@@ -115,7 +115,7 @@ class QAgent():
                 n_obs.append(j)
         #print("n_obs_pre = ", n_obs)
         for c,i in enumerate(n_obs):
-            if c < len(n_obs):
+            if c < len(n_obs)-1:
                 print("c=",c," i=",i ," min[",c,"]=",self.min[c]," max[",c,"]=",self.max[c])
                 n_obs[c]=((2.0 * (i - self.min[c]) / (self.max[c] - self.min[c])) - 1)
         #print("n_obs_post = ", n_obs)
