9721015eb11f86525dd63a77c66243648c34a9ab
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-02 01:24:45 -0500

    Update q_agent_test.py

--- q_agent_test.py
+++ q_agent_test.py
@@ -113,7 +113,7 @@ class QAgent():
         # the sum of the same sum from the validation set.
         a_pattern = 0
         
-        for i in range(0, (self.vs_num_columns-4)/self.obs_ticks):
+        for i in range(0, (self.vs_num_columns-4)/self.obsticks):
             print("num_columns=",self_vs,"len(obs)=",len(normalized_observation), "i=",i)
             a_pattern = a_pattern + normalized_observation[self.obsticks * i]
         #  for each row of the validation set(output of q-datagen), do the sum and compare with the observation sum
