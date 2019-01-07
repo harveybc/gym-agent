7767bf1fda24a9c69c792ea3fd8bd96f73583b6c
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-07 07:20:06 -0500

    Update q_agent_test_v2.py

--- q_agent_test_v2.py
+++ q_agent_test_v2.py
@@ -216,9 +216,9 @@ class QAgent():
                     vol = dir * self.raw_action[2] * (1 - self.security_margin)
  # TODO: by setting the following to an unreachable condition 2.0, only allow close by sl/tp                       
         if order_status == 1:
-            # if TP, SL, dInv and direction son negativos, retorna los valores ajustados con el margen para sell order
-            if (self.raw_action[0] < 0) and (self.raw_action[1] < 0) and (self.raw_action[2] < 0) and (self.raw_action[3] < 0):
-                # opens buy order  
+            # if TP, SL, dInv or direction son negativos, retorna los valores ajustados con el margen para sell order
+            if (self.raw_action[0] <= 0) or (self.raw_action[1] <= 0) or (self.raw_action[2] <= 0) or (self.raw_action[3] <= 0):
+                # closes buy order  
                 dir = -1
                 # TP
                 if self.raw_action[0] < -1:
@@ -238,8 +238,8 @@ class QAgent():
  # TODO: by setting the following to an unreachable condition -2.0, only allow close by sl/tp                   
         if order_status == -1:
             # if TP, SL, dInv and direction son positivos, retorna los valores ajustados con el margen para buy order
-            if (self.raw_action[0] > 0) and (self.raw_action[1] > 0) and (self.raw_action[2] > 0) and (self.raw_action[3] > 0):
-                # opens buy order  
+            if (self.raw_action[0] >= 0) or (self.raw_action[1] >= 0) or (self.raw_action[2] >= 0) or (self.raw_action[3] >= 0):
+                # closes sell order  
                 dir = 1
                 # TP
                 if self.raw_action[0] > 1:
