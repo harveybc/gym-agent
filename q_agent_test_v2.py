5879be679c15821dc1e768f13eddbc523fd77955
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-07 07:40:34 -0500

    Update q_agent_test_v2.py

--- q_agent_test_v2.py
+++ q_agent_test_v2.py
@@ -59,9 +59,9 @@ class QAgent():
         self.obsticks = 15
         # TODO: obtener min y max de actions from q-datagen dataset headers
         self.min_TP = 100
-        self.max_TP = 10000
+        self.max_TP = 1000
         self.min_SL = 100
-        self.max_SL = 10000
+        self.max_SL = 1000
         self.min_volume = 0.0
         self.max_volume = 0.02
         self.security_margin = 0.4
