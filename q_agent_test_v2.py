3e91817e05946508ebad69cb72ca1122610482a4
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-07 04:26:20 -0500

    Update q_agent_test_v2.py

--- q_agent_test_v2.py
+++ q_agent_test_v2.py
@@ -63,7 +63,7 @@ class QAgent():
         self.min_SL = 100
         self.max_SL = 10000
         self.min_volume = 0.0
-        self.max_volume = 0.2
+        self.max_volume = 0.02
         self.security_margin = 0.2
         
         # register the gym-forex openai gym environment
