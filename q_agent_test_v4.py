6bc6b1b1fa22d6233f566da280af4cb9d9ae1321
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-30 15:59:45 -0500

    Update q_agent_test_v4.py

--- q_agent_test_v4.py
+++ q_agent_test_v4.py
@@ -196,16 +196,19 @@ class QAgent():
         #apply pre-processing
         n_obs = self.pt.transform(np.array(n_obs).reshape(1,-1))
         n_o = n_obs.tolist()
+        n_o=n_o[0]
         #print("n_o=",n_o)
         
         # append 10 columns used to preprocess test training signals TODO: QUITAR DE AQUI Y DEL DATASET
         for i in range(0,9):
-            n_o[0].append(0)
+            n_o.append(0)
         #apply feature selection.
-        print("n_o[0]=",n_o[0])
+        print("n_o=",n_o)
         print("mask=",self.mask)
+        print("len(n_o)=",len(n_o))
+        print("len(mask)", len(mask))
         
-        n_obs = n_o[0][self.mask]
+        n_obs = n_o[self.mask]
     
         return n_obs
     
