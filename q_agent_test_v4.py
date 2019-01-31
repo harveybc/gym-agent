4ae5f2c7857990f335207a9ac34e5f5ced122195
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-31 17:42:45 -0500

    Update q_agent_test_v4.py

--- q_agent_test_v4.py
+++ q_agent_test_v4.py
@@ -135,7 +135,7 @@ class QAgent():
         # the sum of the same sum from the validation set.
         a_pattern = 0
         # TODO: ERROR: NO COINCIDEN EL RETURN DE ENTRENAMIENTO CON EL CALCULADO EN NORMALIZE OBS
-        num_features = ((self.vs_num_columns//2)-self.num_s)//self.obsticks
+        num_features = ((self.vs_num_columns//8)-self.num_s)//self.obsticks
         n_p = -1
         for i in range(0, num_features):
             n_p = n_p * -1
