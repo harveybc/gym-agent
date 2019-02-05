5716ee25986f8a41951830298d823bcdf9a7200d
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-02-04 22:25:01 -0500

    Update q_agent_test_v4.py

--- q_agent_test_v4.py
+++ q_agent_test_v4.py
@@ -129,7 +129,7 @@ class QAgent():
         # evaluate all models with the observation data window 
         self.action = []
         self.max_index = 0 
-        action_list = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        action_list = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
         vs = np.array(normalized_observation)
         # read the normalized_observation skipping (num_features-1) and sum the values to compare with
         # the sum of the same sum from the validation set.
@@ -160,7 +160,7 @@ class QAgent():
                     a_search = a_search * self.vs_data[i, self.obsticks * j]
             # Return all values from the action signals
             if a_pattern == a_search:
-                action_list_n = self.vs_data[i, self.vs_num_columns-11 : self.vs_num_columns].copy()
+                action_list_n = self.vs_data[i, self.vs_num_columns-9 : self.vs_num_columns].copy()
                 action_list = action_list_n.tolist()
                 break
         #print("normalized_observation=", normalized_observation)
