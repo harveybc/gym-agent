9c7a66126587c6e82fed2dab93de559740edd8d2
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-05-22 23:01:48 -0500

    Update q_agent_dcn_v3.py

--- q_agent_dcn_v3.py
+++ q_agent_dcn_v3.py
@@ -262,8 +262,8 @@ class QAgent():
         #for i in range(0,10):
         #    n_obs.append(0) 
         #apply pre-processing
-        # n_obs = self.pt.transform(np.array(n_obs).reshape(1,-1))
-        n_obs = np.array(n_obs).reshape(1,-1) 
+        n_obs = self.pt.transform(np.array(n_obs).reshape(1,-1))
+        #n_obs = np.array(n_obs).reshape(1,-1) 
         n_o = n_obs[0].tolist()
         #print("n_o=",n_o)
         #for i in range(0,9):
