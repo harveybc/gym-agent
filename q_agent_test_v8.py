b5d07f89d248b7b4c03fb60fde014f65cf59944a
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-07-03 22:24:47 -0500

    Update q_agent_test_v8.py

--- q_agent_test_v8.py
+++ q_agent_test_v8.py
@@ -262,11 +262,13 @@ class QAgent():
             if (self.raw_action[0] > self.raw_action[3]):
                 # opens buy order  
                 dire = 1.0
-                tp_a = abs(self.raw_action[0]-self.raw_action[3])
+                #tp_a = abs(self.raw_action[0]-self.raw_action[3])
+                tp_a = self.max_tp/2
             if (self.raw_action[0] <= self.raw_action[3]):
                 # opens sell order  
                 dire = -1.0
-                tp_a = abs(self.raw_action[3]-self.raw_action[0])
+                #tp_a = abs(self.raw_action[3]-self.raw_action[0])
+                tp_a = self.max_tp/2
         # if there is an existing buy order
         if (order_status == 1) and (self.duration > self.min_duration):
             # si action[0] == 0 cierra orden de buy 
